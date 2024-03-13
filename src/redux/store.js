100e9c7305f52cdec82821845a82aa8791698a9d
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-13 12:38:43 +0200

    fix LS

--- src/redux/store.js
+++ src/redux/store.js
@@ -24,7 +24,7 @@ import {
   const counterPersistConfig = {
     key: 'counter',
     storage,
-    whitelist: ['daylynorma'],
+    whitelist: ['dailyNorma'],
   };
   
   const dataPersistConfig = {
