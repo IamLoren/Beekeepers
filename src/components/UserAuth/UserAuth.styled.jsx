29ce70d65dccbcc9c59e24fed0fee2aa374570f1
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2024-03-13 16:59:52 +0100

    feature/header

--- src/components/UserAuth/UserAuth.styled.jsx
+++ src/components/UserAuth/UserAuth.styled.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const StyledUserAuthWrapper = styled.div`
@@ -5,6 +6,10 @@ export const StyledUserAuthWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   gap: 2px;
+`;
+
+export const StyledLink = styled(Link)`
+  color: var(--secondary-text);
 `;
 
 export const StyledSvg = styled.svg`
