019a6b85a494ecd201e2339b9cb160b0de3700c3
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-13 16:42:04 +0200

    added Containet to Homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,26 @@
+import Container from '../../components/Container/Container';
 import DailyNorma from '../../components/DailyNorma/DailyNorma';
 import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
 import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
+import { StyledFlexContainer } from './HomePage.styled';
 
 const HomePage = () => {
   return (
     <div>
-      <div>
-        <DailyNorma />
-        <WaterRatioPanel />
-      </div>
-      <div>
-        <TodayWaterList />
-        <MonthStatsTable />
-      </div>
+      <Container>
+        <StyledFlexContainer>
+          <div>
+          <DailyNorma />
+          <WaterRatioPanel />
+        </div>
+        <div>
+          <TodayWaterList />
+          <MonthStatsTable />
+        </div>
+        </StyledFlexContainer>
+        
+      </Container>
     </div>
   );
 };
