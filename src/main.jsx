0c1c46252e6076f9996068113fb177abc2507748
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-13 15:50:06 +0200

    added common styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
-import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
+import Global from './css/common.js';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/Beekeepers">
       <Provider store={store}>
-         <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Global />
+      </PersistGate>
+      <ToastContainer autoClose={1500} />
       </Provider> 
     </BrowserRouter>
   </React.StrictMode>,
