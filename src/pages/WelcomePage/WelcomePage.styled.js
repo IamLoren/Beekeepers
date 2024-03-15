8bf35fc33bead3aefeaf87489366cc78b775ec2c
kkot04 <144848298+kkot04@users.noreply.github.com>
2024-03-15 05:38:17 +0200

    markup

---
+++ src/pages/WelcomePage/WelcomePage.styled.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const StyledFullPage = styled.div`
+
+`
+
+export const StyledContainer = styled.div`
+
+`
+
+export const StyledLeftWrapper = styled.div`
+
+`
+
+export const StyledTitle1 = styled.h1`
+
+`
+
+export const StyledTitle2 = styled.h2`
+
+`
+export const StyledLeftTitle3 = styled.h3`
+
+`
+
+export const StyledLeftList = styled.ul`
+
+`
+
+export const StyledLeftItem = styled.li`
+
+`
+
+export const StyledButton = styled.button`
+
+`
+
+export const StyledRightWrapper = styled.div`
+
+`
+
+export const StyledRightTitle3 = styled.h3`
+
+`
+
+export const StyledRightList = styled.ul`
+
+`
+
+export const StyledRightItem = styled.li`
+
+`
