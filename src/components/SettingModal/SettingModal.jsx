c1957e99f9e2ce65d2ec18fd8191da63a21be1a5
khrystyna-shekel <christina.shekel@gmail.com>
2024-03-15 22:09:50 +0200

    plus

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,4 +1,4 @@
-import { Radio, RadioGroup } from '@mui/material';
+// import { Radio, RadioGroup } from '@mui/material';
 import sprite from '../../assets/sprite.svg';
 
 const SettingModal = () => {
@@ -6,42 +6,46 @@ const SettingModal = () => {
     <div>
       <h1>Setting</h1>
 
-      <div>
-        <h2>Your photo</h2>
-        <img src="/" alt="avatar" />
-        <button>
-          <svg className="arrow-up" fill="none">
-            <use href={sprite + '#icon-arrow-up-try'}></use>
-          </svg>
-          Upload a photo
-        </button>
-      </div>
-
       <form>
+        <div>
+          <h2>Your photo</h2>
+          <img src="/" alt="avatar" />
+          <button>
+            <svg className="arrow-up" fill="none">
+              <use href={sprite + '#icon-arrow-up-try'}></use>
+            </svg>
+            Upload a photo
+          </button>
+        </div>
+
         <h2>Your gender identity</h2>
-        <RadioGroup defaultValue="woman" name="gender" row>
+        {/* <RadioGroup defaultValue="woman" name="gender" row>
           <Radio value="woman" label="Woman" />
           <Radio value="man" label="Man" />
-        </RadioGroup>
+        </RadioGroup> */}
 
-        <label htmlFor="">
+        <label htmlFor="name">
           <h2>Your name</h2>
-          <input type="text" placeholder="David" />
+          <input id="name" type="text" placeholder="David" />
         </label>
 
-        <label htmlFor="">
+        <label htmlFor="email">
           <h2>E-mail</h2>
-          <input type="text" placeholder="david01@gmail.com" />
+          <input id="email" type="text" placeholder="david01@gmail.com" />
         </label>
 
         <h2>Password</h2>
-        <label htmlFor="">
-          <p>Outdated password:</p>
-          <input type="password" />
-          <p>New Password:</p>
-          <input type="password" />
-          <p>Repeat new password:</p>
-          <input type="password" />
+        <label htmlFor="oldPassword">
+          Outdated password:
+          <input id="oldPassword" type="password" />
+        </label>
+        <label htmlFor="newPassword">
+          New Password:
+          <input id="newPassword" type="password" />
+        </label>
+        <label htmlFor="repeatPassword">
+          Repeat new password:
+          <input id="repeatPassword" type="password" />
         </label>
         <button type="submit">Save</button>
       </form>
