368850eae7c9e9b0e7f514c32b01515af7dd6bc0
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2024-03-15 19:12:41 +0100

    feature/modal

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import UserLogoutModal from '../UserLogoutModal/UserLogoutModal';
 import sprite from '../../assets/sprite.svg';
 import {
   StyledButton,
@@ -7,47 +5,47 @@ import {
   StyledSpan,
   StyledSvg,
 } from './UserLogoModal.styled';
+import { useDispatch } from 'react-redux';
+import {
+  changeLogoutModal,
+  changeModalOpen,
+  changeSettingModal,
+} from '../../redux/modals/modalsSlice';
 
 const UserLogoModal = () => {
-  // const [isUserInfoModalOpen, setIsUserInfoModalOpen] = useState(false);
-  const [isUserLogoutModalOpen, setIsUserLogoutModalOpen] = useState(false);
+  const dispatch = useDispatch();
 
   const handleSettingClick = () => {
-    // setIsUserInfoModalOpen(true);
+    dispatch(changeModalOpen(true));
+    dispatch(changeSettingModal(true));
   };
 
   const handleLogoutClick = () => {
-    setIsUserLogoutModalOpen(true);
+    dispatch(changeModalOpen(true));
+    dispatch(changeLogoutModal(true));
   };
 
   return (
-    <StyledModalWrapper>
-      {/* {isUserInfoModalOpen && (
-        <SettingModal onClose={() => setIsUserInfoModalOpen(false)} />
-      )} */}
-      {isUserLogoutModalOpen && (
-        <UserLogoutModal onClose={() => setIsUserLogoutModalOpen(false)} />
-      )}
-      <StyledButton onClick={handleSettingClick}>
-        <StyledSpan>
-          {' '}
-          <StyledSvg width={16} height={16}>
-            <use href={`${sprite}#icon-cog-6-tooth`} />
-          </StyledSvg>
-        </StyledSpan>
-        Setting
-      </StyledButton>
-      <StyledButton onClick={handleLogoutClick}>
-        {' '}
-        <StyledSpan>
-          {' '}
-          <StyledSvg width={16} height={16}>
-            <use href={`${sprite}#icon-arrow-right-on-rectangle`} />
-          </StyledSvg>
-        </StyledSpan>
-        Log out
-      </StyledButton>
-    </StyledModalWrapper>
+    <>
+      <StyledModalWrapper>
+        <StyledButton onClick={handleSettingClick}>
+          <StyledSpan>
+            <StyledSvg width={16} height={16}>
+              <use href={`${sprite}#icon-cog-6-tooth`} />
+            </StyledSvg>
+          </StyledSpan>
+          Setting
+        </StyledButton>
+        <StyledButton onClick={handleLogoutClick}>
+          <StyledSpan>
+            <StyledSvg width={16} height={16}>
+              <use href={`${sprite}#icon-arrow-right-on-rectangle`} />
+            </StyledSvg>
+          </StyledSpan>
+          Log out
+        </StyledButton>
+      </StyledModalWrapper>
+    </>
   );
 };
 
