07f634957d20c955cc9f07e9857f14a52676197c
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-17 00:59:56 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Beekeepers">
+    <BrowserRouter >
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
