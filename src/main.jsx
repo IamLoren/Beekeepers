da397c89c9c6678d3c0c776d3932a3ef54d915c6
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-17 11:47:07 +0200

    fix reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -10,8 +9,7 @@ import Global from './css/common.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
@@ -20,5 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <ToastContainer autoClose={1500} />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
 );
