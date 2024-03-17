dfefbfcd2140cd76de23cda8d29753eac64513c8
VButenk0 <vladflash10@gmail.com>
2024-03-17 18:55:07 +0200

    added thunks and extraReducers for statistic slice

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -16,70 +16,70 @@ export const authSlice = createSlice({
       gender: '',
     },
     token: '',
-    isLogged: true,
+    isLogged: false,
     isLoading: false,
     isRefresh: false,
     isError: null,
   },
   reducers: {},
 
-    extraReducers: builder => {
-      builder
-        .addCase(registerThunk.fulfilled, (state, { payload }) => {
-          console.log(payload)
-          state.user.email = payload.email;
-          state.token = payload.token;
-          state.isLogged = true;
-          state.isLoading = false;
-        })
-        .addCase(registerThunk.rejected, (state) => {
-          toast.error('Error! User exist!');
-          state.isLogged = false;
-          state.isLoading = false;
-        })
-        .addCase(loginThunk.rejected, (state) => {
-          toast.error('Error! Something went wrong!');
-          state.isLogged = false;
-          state.isLoading = false;
-        })
-        .addCase(loginThunk.fulfilled, (state, { payload }) => {
-          state.user.email = payload.user.email;
-          state.user.name = payload.user.username;
-          state.user.balance = payload.user.balance;
-          state.token = payload.token;
-          state.isLogged = true;
-          state.isLoading = false;
-        })
-        .addCase(logoutThunk.fulfilled, state => {
-          state.user = {
-            email: '',
-            name: '',
-          };
-          state.token = '';
-          state.isLogged = false;
-          state.isLoading = false;
-        })
-        .addCase(loginThunk.pending, state => {
-          state.isLoading = true;
-        })
-        .addCase(refreshThunk.fulfilled, (state, { payload }) => {
-          state.user.name = payload.username;
-          state.user.email = payload.email;
-          state.user.balance = payload.balance;
-          state.isLogged = true;
-          state.isLoading = false;
-        })
-        .addCase(refreshThunk.pending, state => {
-          state.isLoading = true;
-          state.isLogged = true;
-          state.isRefresh = true;
-        })
-        .addCase(refreshThunk.rejected, (state) => {
-          toast.error('You need to logIn!');
-          state.isLogged = false;
-          state.isLoading = false;
-        })
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerThunk.fulfilled, (state, { payload }) => {
+        console.log(payload);
+        state.user.email = payload.email;
+        state.token = payload.token;
+        state.isLogged = true;
+        state.isLoading = false;
+      })
+      .addCase(registerThunk.rejected, (state) => {
+        toast.error('Error! User exist!');
+        state.isLogged = false;
+        state.isLoading = false;
+      })
+      .addCase(loginThunk.rejected, (state) => {
+        toast.error('Error! Something went wrong!');
+        state.isLogged = false;
+        state.isLoading = false;
+      })
+      .addCase(loginThunk.fulfilled, (state, { payload }) => {
+        state.user.email = payload.user.email;
+        state.user.name = payload.user.username;
+        state.user.balance = payload.user.balance;
+        state.token = payload.token;
+        state.isLogged = true;
+        state.isLoading = false;
+      })
+      .addCase(logoutThunk.fulfilled, (state) => {
+        state.user = {
+          email: '',
+          name: '',
+        };
+        state.token = '';
+        state.isLogged = false;
+        state.isLoading = false;
+      })
+      .addCase(loginThunk.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(refreshThunk.fulfilled, (state, { payload }) => {
+        state.user.name = payload.username;
+        state.user.email = payload.email;
+        state.user.balance = payload.balance;
+        state.isLogged = true;
+        state.isLoading = false;
+      })
+      .addCase(refreshThunk.pending, (state) => {
+        state.isLoading = true;
+        state.isLogged = true;
+        state.isRefresh = true;
+      })
+      .addCase(refreshThunk.rejected, (state) => {
+        toast.error('You need to logIn!');
+        state.isLogged = false;
+        state.isLoading = false;
+      });
+  },
 });
 
 export const authReducer = authSlice.reducer;
--- src/redux/statisticData/statisticDataSlice.js
+++ src/redux/statisticData/statisticDataSlice.js
@@ -1,4 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
+import {
+  fetchPortionsThunk,
+  deletePortionThunk,
+  addPortionThunk,
+  updatePortionThunk,
+  fetchDailyPortionsThunk,
+  fetchMonthlyPortionsThunk,
+} from './operations';
 
 export const statisticDataSlice = createSlice({
   name: 'data',
@@ -11,42 +19,91 @@ export const statisticDataSlice = createSlice({
       { id: 5, amount: 200, time: '14:00' },
       { id: 6, amount: 250, time: '14:00' },
     ],
+    dailyPortions: [],
+    monthlyPortions: [],
     isLoading: false,
     isError: null,
     selectedItem: null,
   },
   reducers: {
-    addPortion: (state, { payload }) => {
-      state.portions.push(payload);
-    },
-    editPortion: (state, { payload }) => {
-      const { id, amount, time } = payload;
-      const portionToUpdate = state.portions.find(
-        (portion) => portion.id === id
-      );
-      if (portionToUpdate) {
-        portionToUpdate.amount = amount;
-        portionToUpdate.time = time;
-      }
-    },
-    deletePortion: (state, { payload }) => {
-      const idToDelete = payload;
-      state.portions = state.portions.filter((item) => item.id !== idToDelete);
-    },
+    // addPortion: (state, { payload }) => {
+    //   state.portions.push(payload);
+    // },
+    // editPortion: (state, { payload }) => {
+    //   const { id, amount, time } = payload;
+    //   const portionToUpdate = state.portions.find(
+    //     (portion) => portion.id === id
+    //   );
+    //   if (portionToUpdate) {
+    //     portionToUpdate.amount = amount;
+    //     portionToUpdate.time = time;
+    //   }
+    // },
+    // deletePortion: (state, { payload }) => {
+    //   const idToDelete = payload;
+    //   state.portions = state.portions.filter((item) => item.id !== idToDelete);
+    // },
     changeSelectedItem: (state, { payload }) => {
       state.selectedItem = payload;
     },
   },
 
-  // extraReducers: (builder) => {
-  //   builder;
-  // .addCase(getNormaThunk.fulfilled, (state, { payload }) => {
-  //     state.dailyNorma = payload;
-  //   state.isLoading = false;
-  // })
-  // },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPortionsThunk.fulfilled, (state, { payload }) => {
+        state.portions = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchDailyPortionsThunk.fulfilled, (state, { payload }) => {
+        state.portions = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchMonthlyPortionsThunk.fulfilled, (state, { payload }) => {
+        state.portions = payload;
+        state.isLoading = false;
+      })
+      .addCase(deletePortionThunk.fulfilled, (state, { payload }) => {
+        state.portions = state.portions.filter(
+          (item) => item.id !== payload.id
+        );
+      })
+      .addCase(addPortionThunk.fulfilled, (state, { payload }) => {
+        state.portions.push(payload);
+      })
+      .addCase(updatePortionThunk.fulfilled, (state, { payload }) => {
+        const { id, amount, time } = payload;
+        const portionToUpdate = state.portions.find(
+          (portion) => portion.id === id
+        );
+        if (portionToUpdate) {
+          portionToUpdate.amount = amount;
+          portionToUpdate.time = time;
+        }
+      })
+      .addMatcher(
+        (action) => action.type.endsWith('/fulfilled'),
+        (state) => {
+          state.isError = false;
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith('/pending'),
+        (state) => {
+          state.isLoading = true;
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith('/rejected'),
+        (state, { payload }) => {
+          state.isLoading = false;
+          state.isError = payload;
+        }
+      );
+  },
 });
 
 export const statisticDataReducer = statisticDataSlice.reducer;
-export const { addPortion, editPortion, deletePortion, changeSelectedItem } =
-  statisticDataSlice.actions;
+export const {
+  // addPortion, editPortion, deletePortion,
+  changeSelectedItem,
+} = statisticDataSlice.actions;
