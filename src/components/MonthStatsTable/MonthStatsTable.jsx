552b6765990a909dc0add2992c2a178f550b3cad
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-17 17:03:11 +0200

    added tooltip

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
-
+import 'react-tooltip/dist/react-tooltip.css';
+import { StyledTooltip } from './MonthStatsTable.styled';
 
 const CustomTile = ({ date, view }) => {
-  
   const tileStyle = {
-    textAlign: 'center', 
+    textAlign: 'center',
     paddingTop: '10px',
-    backgroundColor: 'transparent', 
+    backgroundColor: 'transparent',
     position: 'relative',
   };
 
   const textStyle = {
     fontSize: '0.8em',
-    color: 'skyblue', 
-    backgroundColor:'transparent',
+    color: 'skyblue',
+    backgroundColor: 'transparent',
   };
 
   const circleStyle = {
@@ -26,11 +26,11 @@ const CustomTile = ({ date, view }) => {
     left: '1px',
     zIndex: '1',
     borderRadius: '50%',
-  }
+  };
 
   return (
     <div style={tileStyle}>
-      <div style={textStyle}>{"0%"}</div>
+      <div style={textStyle}>{'0%'}</div>
       <div style={circleStyle}></div>
     </div>
   );
@@ -38,22 +38,34 @@ const CustomTile = ({ date, view }) => {
 
 const MonthStatsTable = () => {
   const [value, setValue] = useState(new Date());
+  
+  const tiles = document.querySelectorAll('.react-calendar__tile');
+  tiles.forEach((button) => {
+    button.setAttribute('data-tooltip-id', 'my-tooltip');
+    button.setAttribute('data-tooltip-content', 'Hello world!');
+  });
 
   function onChange(nextValue) {
     setValue(nextValue);
   }
 
   return (
-    <Calendar
-      onChange={onChange}
-      views={['month', 'tenDays']}
+    <div>
+      <Calendar
+        onChange={onChange}
+        views={['month', 'tenDays']}
         tileWidth={40}
-      value={value}
-      minDate={new Date(2024, 2, 16)}
-      locale="en-GB" 
-      tileContent={({ date, view }) => <CustomTile date={date} view={view} />}
-    />
+        value={value}
+        minDate={new Date(2024, 2, 16)}
+        locale="en-GB"
+        onActiveStartDateChange={({ activeStartDate }) =>
+          setValue(activeStartDate)
+        }
+        tileContent={({ date, view }) => <CustomTile date={date} view={view} />}
+      />
+      <StyledTooltip id="my-tooltip" />
+    </div>
   );
-}
+};
 
-export default MonthStatsTable 
+export default MonthStatsTable;
