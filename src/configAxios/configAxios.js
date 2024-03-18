d87f8e80e64f620fa709c1dde6d6890538074407
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-18 13:21:12 +0200

    fix path to back

--- src/configAxios/configAxios.js
+++ src/configAxios/configAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://byte-water-back.onrender.com',
+  baseURL: 'https://beekeepers-back.onrender.com',
 });
 
 export const setToken = (token) => {
