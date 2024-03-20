6e773bfd3beb975eb7fe02d9e11d174736d7ec91
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2024-03-20 13:27:22 +0100

    add clear reducer

--- src/components/UserLogoutModal/UserLogoutModal.jsx
+++ src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -12,6 +12,8 @@ import {
   StyledTitle,
 } from './UserLogoutModal.styled';
 import { logoutThunk } from '../../redux/auth/operations';
+import { clearStatisticData } from '../../redux/statisticData/statisticDataSlice';
+import { clearNormaCounterData } from '../../redux/normaCounter/normaCounterSlice';
 
 const UserLogoutModal = () => {
   const dispatch = useDispatch();
@@ -23,6 +25,10 @@ const UserLogoutModal = () => {
 
   const handleLogoutClick = () => {
     dispatch(logoutThunk());
+    dispatch(clearStatisticData());
+    console.log(clearStatisticData);
+    dispatch(clearNormaCounterData());
+    console.log(clearNormaCounterData);
     dispatch(changeModalOpen(false));
     dispatch(changeLogoutModal(false));
   };
