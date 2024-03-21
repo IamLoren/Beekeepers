459ac18f8525ef23010858a21575f57677f8a312
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2024-03-21 15:01:34 +0200

    changed time selector and fixed some styles

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,4 @@
 export const selectIsLogged = (state) => state.authSlice.isLogged;
-export const selectPortions = (state) => state.statisticDataSlice.portions;
 export const selectDailyPortions = (state) =>
   state.statisticDataSlice.dailyPortions;
 export const selectMonthlyPortions = (state) =>
@@ -27,7 +26,7 @@ export const selectSelectedItem = (state) =>
   state.statisticDataSlice.selectedItem;
 
 export const selectPortionsAmount = (state) =>
-  state.statisticDataSlice.portions.reduce(
+  state.statisticDataSlice.dailyPortions.reduce(
     (totalAmount, portion) => totalAmount + portion.amount,
     0
   );
