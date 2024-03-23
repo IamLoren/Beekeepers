7cb7005499c5c677d391253d38d99bbabaf7404b
VButenk0 <vladflash10@gmail.com>
2024-03-23 12:55:28 +0200

    added verification page

---
+++ src/pages/VerificationSuccessPage/VerificationSuccessPage.jsx
@@ -0,0 +1,10 @@
+const VerificationSuccessPage = () => {
+  return (
+    <div>
+      <h1>Verified!</h1>
+      <p>You have successfully verified account.</p>
+    </div>
+  );
+};
+
+export default VerificationSuccessPage;
