fa185e2856cc8207622989228c9657c6a2600b30
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-24 04:14:42 +0200

    added button styles

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,28 @@
+import { Tooltip } from "react-tooltip";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
 
 
 const Chart = () => {
+    const data = [
+        { name: 'Січ', uv: 100, pv: 50 },
+        { name: 'Лют', uv: 100, pv: 70 },
+        { name: 'Бер', uv: 100, pv: 90 },
+        { name: 'Кві', uv: 100, pv: 60 },
+        { name: 'Тра', uv: 100, pv: 75 },
+        { name: 'Чер', uv: 100, pv: 80 },
+        { name: 'Лип', uv: 100, pv: 85 },
+      ];
   return (
-    <div>Chart</div>
+    <div>
+        <LineChart width={600} height={350} data={data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+        <XAxis dataKey="name" />
+        <YAxis domain={[0, 100]} />
+        <Tooltip />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Line type="monotone" dataKey="uv" stroke="green" yAxisId={0} />
+        <Line type="monotone" dataKey="pv" stroke="blue" yAxisId={0} />
+      </LineChart>
+    </div>
   )
 }
 
