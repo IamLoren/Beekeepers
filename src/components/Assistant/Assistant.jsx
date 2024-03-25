f481160a3187542f58ac313f15709da0fd84fe82
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-25 03:40:15 +0200

    fix path to styles

--- src/components/Assistant/Assistant.jsx
+++ src/components/Assistant/Assistant.jsx
@@ -1,4 +1,4 @@
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import { MainContainer, ChatContainer,  MessageList, Message, MessageInput, TypingIndicator} from '@chatscope/chat-ui-kit-react';
 import { useState } from 'react';
 
