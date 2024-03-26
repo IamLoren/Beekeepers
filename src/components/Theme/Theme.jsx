2073e1f5138db9954ff8a5df9e6234de12cf86b6
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-26 11:14:38 +0200

    fix saving theme

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,14 +1,24 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectColorTheme } from '../../redux/selectors';
+import { refreshThunk } from '../../redux/auth/operations';
 
 const ThemeClass = () => {
+    const dispatch = useDispatch();
   const currentTheme = useSelector(selectColorTheme);
 
   useEffect(() => {
-    document.documentElement.classList.remove('dark', 'light');
+    const refreshTheme = async() => {
+        try {
+            await dispatch(refreshThunk);
+             document.documentElement.classList.remove('dark', 'light');
     document.documentElement.classList.add(currentTheme);
-  }, [currentTheme]);
+        } catch (error) {
+            console.log(error)
+        } 
+    }
+    refreshTheme()
+  }, [currentTheme, dispatch]);
 
   return null;
 };
